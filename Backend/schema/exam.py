fca208227ad0baeb6541d8761b6d645d8f0caae7
LeeGyeom35 <LeeGyeom35@bctone.kr>
2025-05-13 17:59:56 +0900

    "테스트 문제 문제 생성 후 채점"

--- Backend/schema/exam.py
+++ Backend/schema/exam.py
@@ -1,6 +1,25 @@
 from pydantic import BaseModel
+from typing import Dict
 from typing import Optional
+from datetime import datetime
+
 '''
 class GetTestQuestionRequest(BaseModel):
     subject : str
 '''
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    id: int
+    message: str
+    role: str
+
+class UserData(BaseModel):
+    user: User
+    expires: datetime
+
+class SubmitTestRequest(BaseModel):
+    answers: Dict[str, int]
+    userdata: UserData
