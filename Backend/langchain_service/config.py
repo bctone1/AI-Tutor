5fa26345fcad1726813dbd2bdb7dffaab7657769
LeeGyeom35 <LeeGyeom35@bctone.kr>
2025-06-05 14:35:00 +0900

    2025-06-05 : 프로필 통계

--- Backend/langchain_service/config.py
+++ Backend/langchain_service/config.py
@@ -2,7 +2,7 @@ from langchain_community.chat_models import ChatOpenAI
 from core.config import CHATGPT_API_KEY
 
 llm = ChatOpenAI(
-    model_name="gpt-4",
+    model_name="gpt-4o",
     temperature=0.7,
     openai_api_key = CHATGPT_API_KEY
 )
